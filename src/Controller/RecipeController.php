3495668c4674319a9960106276ac421e6533d349
btomdev <tom.benazerat@gmail.com>
2024-12-14 18:15:24 +0100

    install symfony 7.2
    create first page :
      - home
      - recipe

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recipe', name: 'recipe.index')]
+    public function index(): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+
+    #[Route('/recipe/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9-_]+', 'id' => '\d+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id' => $id,
+        ]);
+    }
+}
